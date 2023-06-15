f300d1a8f6deaa5f534d60637ec55570b188c79a
RAKHYUN <kyle2598@gmail.com>
2023-06-15 06:57:52 -0500

    - build UI in RecipeFeaturedView

--- Recipe List App/Views/RecipeFeaturedView.swift
+++ Recipe List App/Views/RecipeFeaturedView.swift
@@ -8,30 +8,61 @@ struct RecipeFeaturedView: View {
     
     var body: some View {
         
-        GeometryReader { geo in
+        VStack(alignment: .leading, spacing: 0) {
             
-            TabView {
+            Text("Featured Recipes")
+                .bold()
+                .padding(.leading)
+                .padding(.top, 40)
+                .font(.largeTitle)
+            
+            GeometryReader { geo in
                 
-                // Loop through each recipe
-                ForEach (0..<model.recipes.count) { index in
+                TabView {
                     
-                    // Only show those that should be featured
-                    if model.recipes[index].featured == true {
+                    // Loop through each recipe
+                    ForEach (0..<model.recipes.count) { index in
                         
-                        // Recipe card
-                        Rectangle()
+                        // Only show those that should be featured
+                        if model.recipes[index].featured == true {
+                            
+                            // Recipe card
+                            ZStack {
+                                Rectangle()
+                                    .foregroundColor(.white)
+                                    
+                                
+                                VStack(spacing: 0) {
+                                    Image(model.recipes[index].image)
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fill)
+                                        .clipped()
+                                    Text(model.recipes[index].name)
+                                        .padding(5)
+                                }
+                            }
                             .frame(width: geo.size.width - 40, height: geo.size.height - 100, alignment: .center)
                             .cornerRadius(15)
-                            .shadow(radius: 10)
-                            
+                            .shadow(color: Color(.sRGB, red:0, green: 0, blue: 0, opacity: 0.5), radius: 10, x: -5, y: 5)
+                                
+                        }
                     }
                 }
+                .tabViewStyle(PageTabViewStyle(indexDisplayMode: .automatic))
+                .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
             }
-            .tabViewStyle(PageTabViewStyle(indexDisplayMode: .automatic))
-            .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
+            
+            VStack(alignment: .leading, spacing: 10){
+                Text("Preparation Time:")
+                    .font(.headline)
+                Text("1 hour")
+                
+                Text("Hightlights")
+                    .font(.headline)
+                Text("Healthy, Hearty")
+            }
+            .padding([.leading, .bottom])
         }
-        
-        
     }
 }
 
